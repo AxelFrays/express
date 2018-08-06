c6733610d45d47126bc48036ad49b13395730335
AxelFrays <axel.fraysse@ma2s-drone.fr>
2018-08-06 13:41:10 +0200

    3eme backup

--- routes/analyse.js
+++ routes/analyse.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongo = require('mongodb');
 var assert = require('assert');
 
-var Plotly = require('plotly')("AFraysse","bqS2xkziQ05AD6wAMULG");
+
 
 /* GET home page. */
 
@@ -21,22 +21,7 @@ router.get('/', function(req, res, next) {
                 x.push(item['t']);
             });
             StringData = JSON.stringify(value['values']);
-            console.log(StringData.length);
             console.log(x, y);
-
-            var data = [
-                {
-                    x,
-                    y,
-                    type: 'scatter'
-                }
-            ];
-
-            Plotly.newPlot('myDiv', data);
-            var date = value[values][1]['t'];
-            res.render('analyse', {date: date});
-
-
 
         });
         client.close();
--- routes/carte.js
+++ routes/carte.js
@@ -4,7 +4,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('en cours de construction');
-});
+    mongo.connect('mongodb://127.0.0.1:27017/testgeojson', {useNewUrlParser : true },function(err, client) {
+        assert.equal(null, err);
+        var db = client.db('testgeojson');
+        db.collection('geojson').findOne().then(function(value){
 
+            StringData = JSON.stringify(value);
+            console.log(StringData);
+
+        });
+        client.close();
+    });
+});
 module.exports = router;
